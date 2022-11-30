35fb479dbbdd69683399ebc1227fad94b22c0ecd
alvintwng <72246214+alvintwng@users.noreply.github.com>
2022-11-30 17:38:52 +0800

    Add files via upload

--- SwitchPanels.java
+++ SwitchPanels.java
@@ -1,5 +1,7 @@
-package com.zero1.swing;
+package com.zero1.panelTwo;
 
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JFrame;
@@ -8,15 +10,27 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class SwitchPanels extends JFrame implements ActionListener {
+public class SwitchPanels {
 
     private static final int WIDTH = 500;
     private static final int HEIGHT = 200;
 
-    private JFrame j1 = new JFrame("SW J1"), j2 = new JFrame("SW J2");
-    private JPanel panel1, panel2;
+    private JFrame j1 = new JFrame("SW J1");
+    private JFrame j2 = new JFrame("SW J2");
+    private Panel2left panel1;
+    private JPanel panel2;
+    private JPanel panelButton1;
+    private JPanel panelButton2;
 
     private JTextField name;
+
+    public static String textA2B;
+
+    // To pass the var from j1 to j2
+    public static void getTextA2B(String text) {
+        System.out.println(">>>>>> textA2B:: " + text);
+        textA2B = text;
+    }
 
     public static void main(String args[]) {
         SwitchPanels sw1 = new SwitchPanels();
@@ -25,45 +39,55 @@ public class SwitchPanels extends JFrame implements ActionListener {
     public SwitchPanels() {
 
         j1.setSize(WIDTH, HEIGHT);
+        j1.setLayout(new BorderLayout());
         j1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         j2.setSize(WIDTH, HEIGHT);
+        j2.setLayout(new BorderLayout());
         j2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        panel1 = new JPanel();
+        panel1 = new Panel2left();
         panel1.add(new JLabel("Panel 1"));
-
-        JButton button1 = new JButton("callJ2");
-        button1.addActionListener(this);
-        panel1.add(button1);
 
         panel2 = new JPanel();
         panel2.add(new JLabel("Panel 2"));
-
-        JButton button2 = new JButton("callJ1");
-        button2.addActionListener(this);
-        panel2.add(button2);
-
         name = new JTextField(30);
         panel2.add(name);
 
-        j1.add(panel1);
-        j1.setVisible(true);
-        j2.add(panel2);
-        //j2.setVisible(true);
-    }
+        class PanelButton extends JPanel implements ActionListener {
+            PanelButton(String s) {
+                super();
+                setLayout(new FlowLayout());
+                JButton b = new JButton(s);
+                b.addActionListener(this);
+                add(b);
+            }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        String buttonString = e.getActionCommand();
-        if (buttonString.equals("callJ2")) {
-            j2.setVisible(true);
-            j1.setVisible(false);
-        } else if (buttonString.equals("callJ1")) {
-            j1.setVisible(true);
-            j2.setVisible(false);
-            name.setText("j2 " + name.getText());
-        } else {
-            System.out.println("Unexpected error.");
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String buttonString = e.getActionCommand();
+                switch (buttonString) {
+                    case "callJ2" -> {
+                        j2.setVisible(true);
+                        j1.setVisible(false);
+                    }
+                    case "callJ1" -> {
+                        j1.setVisible(true);
+                        j2.setVisible(false);
+                        name.setText("j2 " + name.getText());
+                    }
+                    default -> System.out.println("Unexpected error.");
+                }
+            }
         }
+
+        PanelButton panelButton1 = new PanelButton("callJ2");
+        PanelButton panelButton2 = new PanelButton("callJ1");
+
+        j1.add(panel1, BorderLayout.CENTER);
+        j1.setVisible(true);
+        j1.add(panelButton1, BorderLayout.SOUTH);
+        j2.add(panel2, BorderLayout.CENTER);
+        j2.add(panelButton2, BorderLayout.SOUTH);
+        //j2.setVisible(true);
     }
 }
